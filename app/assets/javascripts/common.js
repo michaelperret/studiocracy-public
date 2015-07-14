db4d45be0624002998bd88aad7521abf9363306a
Brock Whittaker <bjwhitta@asu.edu>
2015-07-14 15:52:39 -0600

    Using a document eventListener to check for 'DOMContentLoaded' to execute function `ready` instead of using jQuery `$(document).ready(ready)` which requires much more overhead.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,6 +2,14 @@ var ready = function() {
 
 };
 
+document.addEventListener('DOMContentLoaded', ready);
+/* not necessary -- DOMContentLoaded is supported in IE8+, Safari, Chrome, FF, Opera, standard browsers. */
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+(function pageLoad() {
+    ready();
+})(); /* either use self-invoking pageLoad function */
+
+$(document).on('page:change', ready); /* for rails-specific (legal) implementation */
+
+//$(document).ready(ready);
+//$(document).on('page:change', ready);
