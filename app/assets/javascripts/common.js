38ee61b4f474c21e9f78b72027d61d04e12e66fd
brockwhittaker <bjwhitta@asu.edu>
2015-07-14 20:24:50 -0600

    Deleting Old Code
    Just cleaning the old code up.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -3,13 +3,9 @@ var ready = function() {
 };
 
 document.addEventListener('DOMContentLoaded', ready);
-/* not necessary -- DOMContentLoaded is supported in IE8+, Safari, Chrome, FF, Opera, standard browsers. */
 
 (function pageLoad() {
     ready();
 })(); /* either use self-invoking pageLoad function */
 
 $(document).on('page:change', ready); /* for rails-specific (legal) implementation */
-
-//$(document).ready(ready);
-//$(document).on('page:change', ready);
