3481a700a2127e22aed72f14d2df83405824c133
Jordan Parra <japarra4@hotmail.com>
2015-07-25 01:29:10 +0000

    add registration form modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,22 @@ module ApplicationHelper
       @title = "#{value} | Studiocracy"
     end
   end
+  
+  def resource_name
+
+    :user
+  
+  end
+  
+  def resource
+  
+    @resource ||= User.new
+  
+  end
+  
+  def devise_mapping
+  
+    @devise_mapping ||= Devise.mappings[:user]
+  
+  end
 end
